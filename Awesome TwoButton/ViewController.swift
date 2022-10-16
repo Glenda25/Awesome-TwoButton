293daf50d9118680b41b894a1985e5446ce3b2f5
Glenda Mullan <glenda.mullan@gmail.com>
2022-10-16 14:40:40 +1100

    2. Complete

--- Awesome TwoButton/ViewController.swift
+++ Awesome TwoButton/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var total = 0
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.text = "0"
     }
 
-
+    @IBAction func addOneButton(_ sender: UIButton) {
+        total = total + 1
+        messageLabel.text = String(total)
+        
+    }
+    
+    @IBAction func addTwoButton(_ sender: UIButton) {
+        total = total + 2
+        messageLabel.text = String(total)
+    }
+    
 }
 
